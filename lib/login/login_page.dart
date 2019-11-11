d844617317590ae14874f8f5669bb2c92f500d3e
Flowmaster <ellyssin.gimhae@gmail.com>
2019-11-10 18:32:15 -0700

    Mobile login, spash, logout using bloc pattern for state management.

---
+++ lib/login/login_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_page/auth/user_repository.dart';
+import 'package:login_page/auth/authentication.dart';
+import 'package:login_page/login/login.dart';
+
+class LoginPage extends StatelessWidget {
+  final UserRepository userRepository;
+
+  LoginPage({Key key, @required this.userRepository})
+      : assert(userRepository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: BlocProvider(
+        builder: (context) {
+          return LoginBloc(
+            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+            userRepository: userRepository,
+          );
+        },
+        child: LoginForm(),
+      ),
+    );
+  }
+}
